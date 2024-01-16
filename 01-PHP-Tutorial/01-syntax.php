9c02752dab0d350c453737ab4f70aec621595ff9
fairuzulum <usleunmdi32@gmail.com>
2024-01-17 01:05:01 +0700

    php syntax

---
+++ 01-PHP-Tutorial/01-syntax.php
@@ -0,0 +1,18 @@
+<?php
+
+/* In PHP, keywords (e.g. if, else, while, echo, etc.),
+   classes, functions, and user-defined functions are 
+   not case-sensitive.
+*/
+echo "Fairuz";
+// same as
+ECHO "Fariuz";
+
+
+//  all variable names are case-sensitive!
+$color = 'blue';
+$COLOR = 'red';
+echo $color;
+echo $COLOR;
+
+?>
